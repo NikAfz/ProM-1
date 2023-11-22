92a4b64adc2645162ce27af23874228c9e96cbe3
nik afz <nikafz980@gmail.com>
2023-11-23 01:07:40 +0400

    added : background

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,49 @@ import ToDo from './camponents/ToDo'
 import AddToDo from './camponents/AddToDo'
 import data from './data'
 import { useEffect, useState } from 'react'
+import Blobs from './camponents/blobs'
+
+
 function App() {
   
+  const [dataState, setData] = useState(data)
+
   const [render,setRender] = useState(false)
-  const renderToDo = data.map((prop) => {
-    return (
-      <ToDo
-        key = {prop.id}
-        name = {prop.toDoName}
-        innerToDo = {prop.innerToDo}
-        fullObject = {prop}
-        rander = {setRender}
-      />
-    ) 
-    
-  })
 
   useEffect(() => {
-    console.log(data);
-    localStorage.setItem('todoData', JSON.stringify(data))
+    console.log(dataState);
+    localStorage.setItem('todoData', JSON.stringify(dataState));
   }, [render]);
 
+  function RenderToDo(props) {
+    return (
+      <ToDo
+        name={props.name}
+        ToDo={props.todo}
+        fullObject={props.object}
+        rander={setRender}
+      />
+      
+    );
+  }
+  
   return (
     <>
+      <Blobs/>
+
       <Header/>
 
+      {/* {renderToDo} */}
       <div className='todo--container'>
-        {renderToDo}
+        {dataState.map((item) => {
+          return(
+            <RenderToDo key={item.id} object={item} name={item.toDoName} todo={item.innerToDo} />
+          )
+        })}
       </div>
 
       <AddToDo
-        data = {data}
+        data = {dataState}
         rander = {setRender}
       />
     </>
